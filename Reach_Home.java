3c5f0f16798f52a5d9fc9e3c67fd40d5cd19e341
Vinitha Sri Prathipati <119840677+vinithasriprathipati@users.noreply.github.com>
2024-01-26 17:11:00 +0530

    import java.util.Scanner; public class ReachHome{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int max=x*5;         if(max>=y){             System.out.println("YES");         }         else{             System.out.println("NO");         }     } }

---
+++ Reach_Home.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ReachHome{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int max=x*5;
+        if(max>=y){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
